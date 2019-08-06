457dfde780157457fe3552283b74c13570f494e2
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-08-06 12:56:58 +0300

    Update state and also make the HTTP requestz

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,46 +1,29 @@
-import React, { Component } from 'react'
-import UserItem from './UserItem'
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'judeinno',
-        avatar_url:
-          'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584__340.png',
-        html_url: 'https://github.com/judeinno',
-      },
-      {
-        id: '2',
-        login: 'bruce',
-        avatar_url:
-          'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584__340.png',
-        html_url: 'https://github.com/bruce',
-      },
-      {
-        id: '3',
-        login: 'mark',
-        avatar_url:
-          'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584__340.png',
-        html_url: 'https://github.com/mark',
-      }
-    ]
-  }
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.state.users.map(user =>
-          <UserItem key={user.id} user={user} />
-        )}
-      </div>
-    )
-  }
-}
+import React from 'react';
+import PropTypes from 'prop-types'
+import UserItem from './UserItem';
+import Spinner from '../commons/Spinner';
+
+const Users = props => {
+	return (
+		<div style={userStyle}>
+			{props.loading ? (
+				<Spinner />
+			) : (
+				props.users.map(user => <UserItem key={user.id} user={user} />)
+			)}
+		</div>
+	);
+};
 
 const userStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '1rem'
+	display: 'grid',
+	gridTemplateColumns: 'repeat(3, 1fr)',
+	gridGap: '1rem',
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
 }
 
-export default Users
+export default Users;
