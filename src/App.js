ceb5033a05597261e69c62823b089e87a770c887
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-08-06 11:47:45 +0300

    Show how components and props work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/layout/NavBar'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <h1>Hello from react</h1>
       </div>
     );
