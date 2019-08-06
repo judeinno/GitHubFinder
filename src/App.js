9782d69f353dd0f73c5ed2a8e5ab7dfb0c563d90
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-08-06 13:04:20 +0300

    Add git hub environment variable

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/layout/NavBar'
-import Users from './components/users/Users'
+import NavBar from './components/layout/NavBar';
+import Users from './components/users/Users';
 import axios from 'axios';
 
 class App extends React.Component {
-  state = {
-    users: [],
-    loading: false
-  }
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const res = await axios.get('https://api.github.com/users');
-    this.setState({ users: res.data, loading: false })
-  }
-  render() {
-    return (
-      <div className="App">
-        <NavBar />
-        <div className="container">
-          <Users loading={this.state.loading} users={this.state.users} />
-        </div>
-      </div>
-    );
-  }
-
+	state = {
+		users: [],
+		loading: false,
+	};
+	async componentDidMount() {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/users?client_id=${process.env
+				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env
+				.REACT_APP_GITHUB_CLIENT_SECRET}`,
+		);
+		this.setState({ users: res.data, loading: false });
+	}
+	render() {
+		return (
+			<div className="App">
+				<NavBar />
+				<div className="container">
+					<Users loading={this.state.loading} users={this.state.users} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
